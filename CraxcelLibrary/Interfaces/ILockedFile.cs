5acfff314cf8dc5428a7259de9f5126ae05b851b
Pete McGranaghan <pete.mcgranaghan@outlook.com>
2020-09-24 00:07:54 +0100

    Adds docstrings to interfaces

--- CraxcelLibrary/Interfaces/ILockedFile.cs
+++ CraxcelLibrary/Interfaces/ILockedFile.cs
@@ -2,12 +2,18 @@
 
 namespace craXcel
 {
+    /// <summary>
+    /// LockedFile Interface that should be implemented by all application specific classes.
+    /// </summary>
     public interface ILockedFile
     {
+        /// <summary>
+        /// The file to be unlocked.
+        /// </summary>
         public FileInfo LockedFile { get; }
 
         /// <summary>
-        /// Unlocks the specified file, saving a copy to the original directory with a "craXcel" suffix.
+        /// Unlocks the specified file, saving a copy to a folder defined by the application's settings.
         /// </summary>
         public void Unlock();
     }
--- CraxcelLibrary/Interfaces/ILogger.cs
+++ CraxcelLibrary/Interfaces/ILogger.cs
@@ -4,12 +4,25 @@ using System.Text;
 
 namespace CraxcelLibrary.Interfaces
 {
+    /// <summary>
+    /// Logger interface, to allow expansion into using different types of logging.
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// A collection of all the log messages added so far.
+        /// </summary>
         public List<string> Log { get; }
 
+        /// <summary>
+        /// Add a new log message to the log.
+        /// </summary>
+        /// <param name="logMessage"></param>
         public void Add(string logMessage);
 
+        /// <summary>
+        /// Saves the collection of log messages.
+        /// </summary>
         public void Save();
     }
 }
